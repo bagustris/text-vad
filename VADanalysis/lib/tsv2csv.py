a31bf1b7f110f84142711f24dad8463f29d1ef8f
bagustris <bagustris@yahoo.com>
2018-07-31 23:08:53 +0900

    init commit, moved from disertasi-ser

---
+++ VADanalysis/lib/tsv2csv.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+import sys
+import csv
+
+tabin = csv.reader(sys.stdin, dialect=csv.excel_tab)
+commaout = csv.writer(sys.stdout, dialect=csv.excel)
+for row in tabin:
+  commaout.writerow(row)
